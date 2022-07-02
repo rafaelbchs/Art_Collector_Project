17f7901888b0ee7ff21ca8eec1fc7075c06677b9
rafaelbchs <rafaelbchacins@gmail.com>
2022-07-02 15:49:24 -0500

    fixed images bug

---
+++ build/precache-manifest.fbfef0c9b37439ab7ec6c59a5300fd31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea84a29faf7e09fbe3aba922995ed1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb3a1cab9e6012fcc7",
+    "url": "/static/js/2.6c99d600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c99d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1af926ad95706407b1",
+    "url": "/static/js/main.a4e50bfb.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,125 +1,166 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 // Don't touch this import
-import { fetchQueryResultsFromTermAndValue } from '../api';
-import Title from './Title';
-
+import { fetchQueryResultsFromTermAndValue } from "../api";
+import Title from "./Title";
 
 const Searchable = (props) => {
-    const {searchTerm, searchValue, setIsLoading, setSearchResults} = props
-    return(
-         <span className="content">
-         <a href="#" onClick={async (event) => {
-            event.preventDefault()
-            setIsLoading(true)
-            try {
-                const result = await fetchQueryResultsFromTermAndValue(searchTerm, searchValue)
-                setSearchResults(result)
-            }
-            catch (error) {
-                console.error(error);
-            } finally{
-                setIsLoading(false);
-            }
-         }}>{searchValue}</a>
-         </span>
-    )
-  
-}
-
+  const { searchTerm, searchValue, setIsLoading, setSearchResults } = props;
+  return (
+    <span className="content">
+      <a
+        href="#"
+        onClick={async (event) => {
+          event.preventDefault();
+          setIsLoading(true);
+          try {
+            const result = await fetchQueryResultsFromTermAndValue(
+              searchTerm,
+              searchValue
+            );
+            setSearchResults(result);
+          } catch (error) {
+            console.error(error);
+          } finally {
+            setIsLoading(false);
+          }
+        }}
+      >
+        {searchValue}
+      </a>
+    </span>
+  );
+};
 
 const Feature = (props) => {
-    const {featuredResult,setIsLoading,setSearchResults} = props
-    if(!featuredResult){
-        return <main id="feature"></main>
-    }
-    const {title, dated, images, primaryimageurl, description, culture, style, technique,
-         medium, dimensions, people, department, division, contact, creditline} = featuredResult
-    return (
+  const { featuredResult, setIsLoading, setSearchResults } = props;
+  if (!featuredResult) {
+    return <main id="feature"></main>;
+  }
+  const {
+    title,
+    dated,
+    images,
+    primaryimageurl,
+    description,
+    culture,
+    style,
+    technique,
+    medium,
+    dimensions,
+    people,
+    department,
+    division,
+    contact,
+    creditline,
+  } = featuredResult;
+  return (
     <main id="feature">
-    <div className="object-feature">
-      <header>
-        <h3>{title}</h3>
-        <h4>{dated}</h4>
-      </header>
-      <section className="facts">
+      <div className="object-feature">
+        <header>
+          <h3>{title}</h3>
+          <h4>{dated}</h4>
+        </header>
+        <section className="facts">
+          {description ? (
+            <React.Fragment>
+              <span className="title">Description</span>
+              <span className="content">{description}</span>
+            </React.Fragment>
+          ) : null}
 
-      { description ? (
-<React.Fragment>
-        <span className="title">Description</span>
-        <span className="content">{description}</span>
-    </React.Fragment>) :null }
-          
-        <span className="title">Culture</span>
-          <Searchable searchValue = {culture}  searchTerm = "culture" setSearchResults={setSearchResults} setIsLoading= {setIsLoading}/> 
-       
-          {technique ? (<React.Fragment>
-        <span className="title">Technique</span>
-         <Searchable searchValue = {technique} searchTerm = "technique" setSearchResults={setSearchResults} setIsLoading= {setIsLoading}/> 
-         </React.Fragment>):null }
+          <span className="title">Culture</span>
+          <Searchable
+            searchValue={culture}
+            searchTerm="culture"
+            setSearchResults={setSearchResults}
+            setIsLoading={setIsLoading}
+          />
 
-         
-         { medium ? ( <React.Fragment>
-        <span className="title">Medium</span>
-        <Searchable searchValue = {medium} searchTerm = "medium" setSearchResults={setSearchResults} setIsLoading= {setIsLoading}/> 
-      </React.Fragment>):null }
+          {technique ? (
+            <React.Fragment>
+              <span className="title">Technique</span>
+              <Searchable
+                searchValue={technique}
+                searchTerm="technique"
+                setSearchResults={setSearchResults}
+                setIsLoading={setIsLoading}
+              />
+            </React.Fragment>
+          ) : null}
 
-    { people ? (
+          {medium ? (
+            <React.Fragment>
+              <span className="title">Medium</span>
+              <Searchable
+                searchValue={medium}
+                searchTerm="medium"
+                setSearchResults={setSearchResults}
+                setIsLoading={setIsLoading}
+              />
+            </React.Fragment>
+          ) : null}
 
-    <React.Fragment>
-    {people.map((person) => {
-        return( 
-        <>
-        <span className="title">Person</span>
-        <Searchable searchValue = {person.name} searchTerm ="person" setSearchResults={setSearchResults} setIsLoading= {setIsLoading}/> 
-       </> 
-       )    
-    })}
-   
-     </React.Fragment>) :null 
-    }
-      
-      { dimensions ? (
-<React.Fragment>
-        <span className="title">Dimensions</span>
-        <span className="content">{dimensions}</span>
-    </React.Fragment>) :null }
+          {people ? (
+            <React.Fragment>
+              {people.map((person) => {
+                return (
+                  <>
+                    <span className="title">Person</span>
+                    <Searchable
+                      searchValue={person.name}
+                      searchTerm="person"
+                      setSearchResults={setSearchResults}
+                      setIsLoading={setIsLoading}
+                    />
+                  </>
+                );
+              })}
+            </React.Fragment>
+          ) : null}
 
-    { department ? (
-<React.Fragment>
-        <span className="title">Department</span>
-        <span className="content">{department}</span>
-    </React.Fragment>) :null }
+          {dimensions ? (
+            <React.Fragment>
+              <span className="title">Dimensions</span>
+              <span className="content">{dimensions}</span>
+            </React.Fragment>
+          ) : null}
 
+          {department ? (
+            <React.Fragment>
+              <span className="title">Department</span>
+              <span className="content">{department}</span>
+            </React.Fragment>
+          ) : null}
 
-        <span className="title">Division</span>
-        <span className="content">{division}</span>
+          <span className="title">Division</span>
+          <span className="content">{division}</span>
 
-    { contact ? (
-<React.Fragment>
-        <span className="title">Contact</span>
-        <span className="content"><a href="#">{contact}</a></span>
-</React.Fragment>) :null }
+          {contact ? (
+            <React.Fragment>
+              <span className="title">Contact</span>
+              <span className="content">
+                <a href="#">{contact}</a>
+              </span>
+            </React.Fragment>
+          ) : null}
 
-        <span className="title">CreditLine</span>
-        <span className="content">{creditline}</span>
-        
-      </section>
+          <span className="title">CreditLine</span>
+          <span className="content">{creditline}</span>
+        </section>
 
-
-      <section className="photos">
-    
-                {images.map((image)=>{
-                  return (
-                    <img src={image.baseimageurl} alt="any" />
-                  )
-                })}
-        
-      </section>
-    </div>
-  </main>
-    )
-
-}
+        {images ? (
+          <React.Fragment>
+            <section className="photos">
+              {images.map((image) => {
+                return <img src={image.baseimageurl} alt="any" />;
+              })}
+            </section>
+          </React.Fragment>
+        ) : null}
+      </div>
+    </main>
+  );
+};
 
 export default Feature;
